530550d0d3c337d25050dc88eaf9ffc17cb95d3d
clark whitehead <cwhitehead@thecrm.com>
2024-10-09 10:19:11 +0000

    add shell files for llama 3.2

---
+++ automate_unsloth_3_2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Define hyperparameter ranges
+lora_r_values=(16)
+lora_alpha_values=(32)
+lora_dropout_values=(0.0)
+lora_target_modules_values=("q_proj,k_proj,v_proj,o_proj")
+use_4bit_quantization_values=(True)
+max_seq_len_values=(1024)
+learning_rate_values=(1e-4)
+lr_scheduler_type_values=("cosine")
+warmup_ratio_values=(0.0)
+max_grad_norm_values=(1.0)
+per_device_train_batch_size_values=(1)
+num_train_epochs=(.5)
+
+# Loop through combinations of values
+for lora_r in "${lora_r_values[@]}"; do
+  for lora_alpha in "${lora_alpha_values[@]}"; do
+    for lora_dropout in "${lora_dropout_values[@]}"; do
+      for lora_target_modules in "${lora_target_modules_values[@]}"; do
+        for use_4bit_quantization in "${use_4bit_quantization_values[@]}"; do
+          for max_seq_len in "${max_seq_len_values[@]}"; do
+            for learning_rate in "${learning_rate_values[@]}"; do
+              for lr_scheduler_type in "${lr_scheduler_type_values[@]}"; do
+                for warmup_ratio in "${warmup_ratio_values[@]}"; do
+                  for max_grad_norm in "${max_grad_norm_values[@]}"; do
+                    for per_device_train_batch_size in "${per_device_train_batch_size_values[@]}"; do
+                      for epoch in "${num_train_epochs[@]}"; do
+                      
+                        # Construct the hyperparameter string
+                        hyperparams="lora_r=$lora_r,lora_alpha=$lora_alpha,lora_dropout=$lora_dropout,lora_target_modules=$lora_target_modules,use_4bit_quantization=$use_4bit_quantization,max_seq_len=$max_seq_len,learning_rate=$learning_rate,lr_scheduler_type=$lr_scheduler_type,warmup_ratio=$warmup_ratio,max_grad_norm=$max_grad_norm,per_device_train_batch_size=$per_device_train_batch_size,num_train_epochs=$epoch"
+
+                        # Call the training script with the current combination of parameters
+                        bash run_unsloth_3_2.sh "$lora_r" "$lora_alpha" "$lora_dropout" "$lora_target_modules" "$use_4bit_quantization" \
+                        "meta-llama/Llama-3.2-1B-Instruct" "$max_seq_len" "$learning_rate" "$lr_scheduler_type" "$warmup_ratio" \
+                        "$max_grad_norm" "$per_device_train_batch_size" 1 "$epoch" "text" "llama-sft-lora-fsdp" True \
+                        "HF_TOKEN_PLACEHOLDER" 1
+
+                        python inf_for_eval.py
+
+                        # Pass the hyperparameter string to score_api.py
+                        python score_api.py --hyperparams "$hyperparams"
+                        
+                      done
+                    done
+                  done
+                done
+              done
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
---
+++ run_unsloth_3_2.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Install required packages
+pip install torch==2.1.1 datasets==2.17.1 scipy==1.12.0 hf_transfer==0.1.5 huggingface-hub==0.25.0 wandb==0.16.3 wheel==0.44.0 transformers==4.44.2 accelerate==0.34.2 peft==0.12.0 "trl<0.9.0" bitsandbytes==0.43.3 deepspeed==0.15.1 einops==0.8.0 sentencepiece==0.2.0 nltk==3.9.1 xformers==0.0.23 flash-attn==2.6.3 --no-build-isolation unsloth@git+https://github.com/unslothai/unsloth.git@79a2112ca4a775ce0b3cb75f5074136cb54ea6df
+
+# Set environment variables
+export HF_HOME='/workspace/persistent'
+export HF_HUB_ENABLE_HF_TRANSFER='True'
+
+# Set default values for arguments (in case not provided)
+lora_r=${1:-16}
+lora_alpha=${2:-32}
+lora_dropout=${3:-0.0}
+lora_target_modules=${4:-"q_proj,k_proj,v_proj,o_proj"}
+use_4bit_quantization=${5:-True}
+model_name_or_path=${6:-"meta-llama/Meta-Llama-3.1-70B-Instruct"}
+max_seq_len=${7:-1024}
+learning_rate=${8:-1e-4}
+lr_scheduler_type=${9:-"cosine"}
+warmup_ratio=${10:-0.0}
+max_grad_norm=${11:-1.0}
+per_device_train_batch_size=${12:-1}
+gradient_accumulation_steps=${13:-1}
+num_train_epochs=${14:-1}
+dataset_text_field=${15:-"text"}
+output_dir=${16:-"llama-sft-lora-fsdp"}
+use_peft_lora=${17:-True}
+hf_token=${18:-"HF_TOKEN_PLACEHOLDER"}
+num_process=${19:-1}
+
+# Run the training script with passed or default arguments
+python train_automated.py \
+--lora_r "$lora_r" \
+--lora_alpha "$lora_alpha" \
+--lora_dropout "$lora_dropout" \
+--lora_target_modules "$lora_target_modules" \
+--use_4bit_quantization "$use_4bit_quantization" \
+--model_name_or_path "$model_name_or_path" \
+--max_seq_len "$max_seq_len" \
+--learning_rate "$learning_rate" \
+--lr_scheduler_type "$lr_scheduler_type" \
+--warmup_ratio "$warmup_ratio" \
+--max_grad_norm "$max_grad_norm" \
+--per_device_train_batch_size "$per_device_train_batch_size" \
+--gradient_accumulation_steps "$gradient_accumulation_steps" \
+--num_train_epochs "$num_train_epochs" \
+--dataset_text_field "$dataset_text_field" \
+--output_dir "$output_dir" \
+--use_peft_lora "$use_peft_lora" \
+--hf_token "$hf_token" \
+--num_process "$num_process" \
+--seed 100 \
+--seed 100 \
+--seed 100 \
+--dataset_name "smangrul/ultrachat-10k-chatml" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train" \
+--logging_steps 1 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "no" \
+--save_strategy "no" \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--packing False \
+--per_device_eval_batch_size 1 \
+--gradient_checkpointing True \
+--use_reentrant False \
+--use_flash_attn True \
+--use_unsloth True
